94e8ab06bdf72998367d310efa741d5d6fb87284
zaidkhajah <zaidkhajah@gmail.com>
2025-04-29 14:25:48 +0300

    refactor: Update event handling for project selection and form visibility

--- src/index.js
+++ src/index.js
@@ -6,8 +6,11 @@ import { Project, setActiveProject, getActiveProject, projects, createProjectHTM
 import todoCardFactory from "./todo-card-factory.js";
 import Todo from "./todo.js";
 
+const content = document.querySelector(".content");
+
 const projectsDiv = document.getElementsByClassName("projects")[0];
-let activeProjectElement = projectsDiv.firstElementChild;
+let activeProjectElement;
+setActiveProjectElement(projectsDiv.firstElementChild);
 const projectContent = document.getElementsByClassName("project-content")[0];
 
 const newProjectForm = document.getElementById("new-project-form");
@@ -17,68 +20,99 @@ const newTodoForm = document.getElementById("new-todo-form");
 projectsDiv.addEventListener("click", event => {
     if (event.target.closest(".add-project")) {
         newProjectForm.classList.remove("hidden");
+        content.classList.add("unfocus");
         return;
     }
-    if (event.target.closest(".project") || event.target.closest(".projects-header")) {
-        activeProjectElement.classList.remove("active");
-        activeProjectElement = event.target.closest(".project") || event.target.closest(".projects-header");
-        activeProjectElement.classList.add("active");
-        console.log(activeProjectElement);
-        if (!(activeProjectElement === projectsDiv.firstElementChild)) {
-            setActiveProject(projects.find( project => activeProjectElement.dataset.id === project.id ));
-            projectContent.firstElementChild.firstElementChild.textContent = getActiveProject().name;
-            projectContent.lastElementChild.append(...getActiveProject().todoList);
-        }
+
+    const projectElement = event.target.closest(".project");
+    const projectHeaderElement = event.target.closest(".projects-header");
+
+    if (activeProjectElement === (projectElement || projectHeaderElement)) return;
+
+    if (projectElement) {
+        clearProjectContent();
+        setActiveProjectElement(projectElement);
+        setActiveProject(projects.find( project => activeProjectElement.dataset.id === project.id ));
+        projectContent.firstElementChild.firstElementChild.textContent = getActiveProject().name;
+        displayProjectContent();
+        return;
+    }
+
+    if (projectHeaderElement) {
+        setActiveProjectElement(projectHeaderElement);
+        return;
     }
 });
 
 projectContent.firstElementChild.addEventListener("click", event => {
-    if (event.target.closest("button")) newTodoForm.classList.remove("hidden");
+    if (event.target.closest("button")) {
+        newTodoForm.classList.remove("hidden");
+        content.classList.add("unfocus");
+    }
 });
 
-newProjectForm.lastElementChild.addEventListener("click", newProjectFormHandler);
-newTodoForm.addEventListener("click", newTodoFormHandler);
+newProjectForm.lastElementChild.addEventListener("click", createFormHandler(addNewProject));
+newTodoForm.lastElementChild.addEventListener("click", createFormHandler(addNewTodo));
 
 
 
-
-
-function newProjectFormHandler(event) {
-    if (event.target.closest("button") === this.firstElementChild) {
-        newProjectForm.classList.add("hidden");
-        const newProject = new Project(
-            newProjectForm.children[1].lastElementChild.value,
-            newProjectForm.children[2].lastElementChild.value,
-            new Date()
-        );
-        projects.push(newProject);
-        projectsDiv.lastElementChild.appendChild(createProjectHTMLElement(newProject));
-    };
-    if (event.target.closest("button") === this.lastElementChild) {
-        newProjectForm.classList.add("hidden");
+function createFormHandler(addNew) {
+    return function(event) {
+        const targetBtn = event.target.closest("button");
+        if (!targetBtn) return;
+        if (targetBtn === this.firstElementChild) {
+            addNew();
+        };
+        this.parentElement.classList.add("hidden");
+        content.classList.remove("unfocus");
     }
 }
 
-function newTodoFormHandler(event) {
-    const targetBtn = event.target.closest("button");
-    if (!targetBtn) return;
-    if (targetBtn === this.lastElementChild.firstElementChild) {
-        const todo = new Todo(createTodoInput(
-            Array.from(this.children).
-            filter(element => element.className === "todo-input").
-            map(element => element.value)
-        ));
-        getActiveProject().add(todo);
-        todoCardFactory.set(todo);
-        const card = todoCardFactory.createTodoCard();
-        projectContent.lastElementChild.appendChild(card);
-    }
-    this.classList.add("hidden");
+function addNewProject() {
+    newProjectForm.classList.add("hidden");
+    const newProject = new Project(
+        newProjectForm.children[1].lastElementChild.value,
+        newProjectForm.children[2].lastElementChild.value,
+        new Date()
+    );
+    projects.push(newProject);
+    projectsDiv.lastElementChild.appendChild(createProjectHTMLElement(newProject));
 }
 
+function addNewTodo() {
+    const todo = new Todo(createTodoInput(
+        Array.from(newTodoForm.children).
+        filter(element => element.className === "todo-input").
+        map(element => element.value)
+    ));
+    getActiveProject().add(todo);
+    todoCardFactory.set(todo);
+    const card = todoCardFactory.createTodoCard();
+    projectContent.lastElementChild.appendChild(card);
+}
 
 function createTodoInput(arr) {
     return {
         title : arr[0], project : getActiveProject(), description : arr[1], dueDate : arr[2], reminderDate : arr[3], priority : Number(arr[4])
     };
+}
+
+function setActiveProjectElement(projectElement) {
+    console.log(activeProjectElement);
+    if (activeProjectElement) activeProjectElement.classList.remove("active");
+    activeProjectElement = projectElement;
+    activeProjectElement.classList.add("active");
+}
+
+function clearProjectContent() {
+    projectContent.lastElementChild.innerHTML = "";
+}
+
+function displayProjectContent() {
+    getActiveProject().todoList.forEach(todo => {
+        todoCardFactory.set(todo);
+        projectContent.lastElementChild.appendChild(
+            todoCardFactory.createTodoCard()
+        );
+    })
 }
